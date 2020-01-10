408a312fb6d88e34b926d59d24bf679ece153fec
Negiiiin <neginbaghbanzadeh.com>
2020-01-10 13:09:44 +0330

    Final Project

--- neural_net_tester.py
+++ neural_net_tester.py
@@ -5,7 +5,8 @@ import sys
 from neural_net import train, test,\
      make_neural_net_basic,\
      make_neural_net_two_layer,\
-     make_neural_net_challenging
+     make_neural_net_challenging,\
+    make_neural_net_two_moons
 
 from neural_net_data import simple_data_sets,\
      harder_data_sets,\
@@ -50,7 +51,7 @@ if __name__=="__main__":
 
         elif test_name == "two_moons":
             # this dataset illustrates the overfitting problem
-            main(make_neural_net_large, two_moons_data_set, max_iterations=1000)
+            main( make_neural_net_two_moons, two_moons_data_set, max_iterations=500)
 
         else:
             print("unrecognized test name %s" %(test_name))
